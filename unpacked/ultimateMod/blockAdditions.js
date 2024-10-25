8751d8db0dd5566b060b7293cd3b63821602074f
cwcinc <ovsecrets10@gmail.com>
2024-10-25 20:57:18 +0100

    touches

--- unpacked/ultimateMod/blockAdditions.js
+++ unpacked/ultimateMod/blockAdditions.js
@@ -1,5 +1,5 @@
 const modVersion = "10.31.2024";
-const modName = "SpookyTrack"
+const modName = "SpookyTrack by cwcinc"
 
 const lbVersion = "0.4.1";
 
@@ -71,64 +71,10 @@ const moddedEnvironment = {
 }
 
 const moddedBlocks = {
-	categories: ["Minecraft", "CustomSigns", "Statues", "RoadSigns", "SpecialBlocks", "Space"],
+	categories: [],
 	modelPaths: [
-		"ultimateMod/models/minecraft.glb",
-		"ultimateMod/models/customSigns.glb",
-		"ultimateMod/models/statues.glb",
-		"ultimateMod/models/roadSigns.glb",
-		"ultimateMod/models/specialBlocks.glb",
-		"ultimateMod/models/space.glb"
 	],
     blocks: [
-		{name: "Grass", category: "Minecraft", blenderSceneName: "Minecraft", id: 180},
-		{name: "Dirt", category: "Minecraft", blenderSceneName: "Minecraft", id: 179},
-		{name: "Stone", category: "Minecraft", blenderSceneName: "Minecraft", id: 181},
-		{name: "Cobble", category: "Minecraft", blenderSceneName: "Minecraft", id: 182},
-		{name: "OakPlank", category: "Minecraft", blenderSceneName: "Minecraft", id: 184},
-		{name: "OakLog", category: "Minecraft", blenderSceneName: "Minecraft", id: 185},
-		//{name: "OakLeaves", category: "Minecraft", blenderSceneName: "Minecraft", id: 186},
-		{name: "Water", category: "Minecraft", blenderSceneName: "Minecraft", id: 183},
-
-		{name: "SignHardFish", category: "CustomSigns", blenderSceneName: "CustomSigns", id: 187},
-		{name: "SignHardFishBig", category: "CustomSigns", blenderSceneName: "CustomSigns", id: 188},
-		{name: "RickrollQR", category: "CustomSigns", blenderSceneName: "CustomSigns", id: 190},
-		
-		{name: "PolyCarStatueGold", category: "Statues", blenderSceneName: "Statues", id: 195},
-		{name: "PolyCarStatueSilver", category: "Statues", blenderSceneName: "Statues", id: 194},
-		{name: "PolyCarStatueBronze", category: "Statues", blenderSceneName: "Statues", id: 191},
-		{name: "KodubStatue", category: "Statues", blenderSceneName: "Statues", id: 212},
-		{name: "StatueBase", category: "Statues", blenderSceneName: "Statues", id: 213},
-		{name: "PolyCar", category: "Statues", blenderSceneName: "Statues", id: 189},
-
-		{name: "StopSignPole", category: "RoadSigns", blenderSceneName: "RoadSigns", id: 192},
-		{name: "StopSignWall", category: "RoadSigns", blenderSceneName: "RoadSigns", id: 193},
-		{name: "SL50", category: "RoadSigns", blenderSceneName: "RoadSigns", id: 196},
-		{name: "SL100", category: "RoadSigns", blenderSceneName: "RoadSigns", id: 197},
-		{name: "SL250", category: "RoadSigns", blenderSceneName: "RoadSigns", id: 198},
-		{name: "SL500", category: "RoadSigns", blenderSceneName: "RoadSigns", id: 199},
-		{name: "SL999", category: "RoadSigns", blenderSceneName: "RoadSigns", id: 200},
-		
-		{name: "StartLine", category: "SpecialBlocks", blenderSceneName: "SpecialBlocks", id: 201, isStart: true},
-		{name: "FinishLine", category: "SpecialBlocks", blenderSceneName: "SpecialBlocks", id: 202, isFinish: true},
-		{name: "BarlessFinish", category: "SpecialBlocks", blenderSceneName: "SpecialBlocks", id: 203, isFinish: true},
-		{name: "UpsideDownFinish", category: "SpecialBlocks", blenderSceneName: "SpecialBlocks", id: 204, isFinish: true},
-		{name: "VerticalFinish", category: "SpecialBlocks", blenderSceneName: "SpecialBlocks", id: 205, isFinish: true},
-		{name: "WallFinish", category: "SpecialBlocks", blenderSceneName: "SpecialBlocks", id: 206, isFinish: true},
-		{name: "CheckpointLine", category: "SpecialBlocks", blenderSceneName: "SpecialBlocks", id: 207, isCheckpoint: true},
-		{name: "BarlessCheckpoint", category: "SpecialBlocks", blenderSceneName: "SpecialBlocks", id: 208, isCheckpoint: true},
-		{name: "UpsideDownCheckpoint", category: "SpecialBlocks", blenderSceneName: "SpecialBlocks", id: 209, isCheckpoint: true},
-		{name: "VerticalCheckpoint", category: "SpecialBlocks", blenderSceneName: "SpecialBlocks", id: 210, isCheckpoint: true},
-		{name: "WallCheckpoint", category: "SpecialBlocks", blenderSceneName: "SpecialBlocks", id: 211, isCheckpoint: true},
-    	
-		{name: "planet1", category: "Space", blenderSceneName: "Space", id: 214},
-		{name: "planet2", category: "Space", blenderSceneName: "Space", id: 215},
-		{name: "planet3", category: "Space", blenderSceneName: "Space", id: 216},
-		{name: "planet4", category: "Space", blenderSceneName: "Space", id: 217},
-		{name: "planet5", category: "Space", blenderSceneName: "Space", id: 218},
-		{name: "planet6", category: "Space", blenderSceneName: "Space", id: 219}
-
-		// next id: 220
 	]
 };
 
