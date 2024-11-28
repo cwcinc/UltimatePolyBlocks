c3f895eaf0356d9f6f357d55157d3b045f594e29
cwcinc <ovsecrets10@gmail.com>
2024-11-28 17:17:10 +0000

    remove mc blocks

--- unpacked/ultimateMod/blockAdditions.js
+++ unpacked/ultimateMod/blockAdditions.js
@@ -107,14 +107,14 @@ const moddedBlocks = {
 		"ultimateMod/models/borderlessroads.glb"
 	],
     blocks: [
-		{name: "Grass", category: "Minecraft", blenderSceneName: "Minecraft", id: 180},
+		/*{name: "Grass", category: "Minecraft", blenderSceneName: "Minecraft", id: 180},
 		{name: "Dirt", category: "Minecraft", blenderSceneName: "Minecraft", id: 179},
 		{name: "Stone", category: "Minecraft", blenderSceneName: "Minecraft", id: 181},
 		{name: "Cobble", category: "Minecraft", blenderSceneName: "Minecraft", id: 182},
 		{name: "OakPlank", category: "Minecraft", blenderSceneName: "Minecraft", id: 184},
 		{name: "OakLog", category: "Minecraft", blenderSceneName: "Minecraft", id: 185},
 		//{name: "OakLeaves", category: "Minecraft", blenderSceneName: "Minecraft", id: 186},
-		{name: "Water", category: "Minecraft", blenderSceneName: "Minecraft", id: 183},
+		{name: "Water", category: "Minecraft", blenderSceneName: "Minecraft", id: 183},*/
 
 		{name: "SignHardFish", category: "CustomSigns", blenderSceneName: "CustomSigns", id: 187},
 		{name: "SignHardFishBig", category: "CustomSigns", blenderSceneName: "CustomSigns", id: 188},
